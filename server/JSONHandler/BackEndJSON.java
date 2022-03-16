5de2bd930f1e32b2483413fd85119552cfea6b2d
kaitlinculligan <81047241+kaitlinculligan@users.noreply.github.com>
2022-03-15 22:39:53 -0600

    start of json stuff

--- server/JSONHandler/BackEndJSON.java
+++ server/JSONHandler/BackEndJSON.java
@@ -1,5 +1,16 @@
 package server.JSONHandler;
 
+//need to get json simple jar
+import org.json.*;
+
 public class BackEndJSON {
-    
+    private RequestParser requestParser;
+
+    public BackEndJSON(){
+        requestParser = new RequestParser();
+    }
+
+    public void handleJSON(){
+
+    }
 }
--- server/JSONHandler/RequestParser.java
+++ server/JSONHandler/RequestParser.java
@@ -2,4 +2,7 @@ package server.JSONHandler;
 
 public class RequestParser {
     
+    public String parseRequest(String request){
+        //statements to determine which service a request should go through
+    }
 }
